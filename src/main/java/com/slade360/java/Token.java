183aba945d0a1ec11079a1828c9f7dc9bf1bbc91
austine <austinndauwa@gmail.com>
2023-07-08 20:49:12 +0300

    got a token asychronously

--- src/main/java/com/slade360/java/Token.java
+++ src/main/java/com/slade360/java/Token.java
@@ -11,6 +11,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 public class Token {
     static String url = "https://accounts.multitenant.slade360.co.ke/oauth2/token/";
@@ -19,17 +20,17 @@ public class Token {
     static String clientSecret = dotenv.get("CLIENT_SECRET"); // Substitute with your client_secret
     static String username = dotenv.get("EMAIL"); // Your email
     static String password = dotenv.get("PASSWORD"); // Your healthcloud account password
-    String responseData =  " ";
+
     public static void getToken(TokenCallback callback) {
         try {
             URL obj = new URL(url);
             HttpURLConnection con = (HttpURLConnection) obj.openConnection();
             con.setRequestMethod("POST");
             String urlParameters = "grant_type=password" +
-                    "&client_id=" + URLEncoder.encode(clientId, "UTF-8") +
-                    "&client_secret=" + URLEncoder.encode(clientSecret, "UTF-8") +
-                    "&username=" + URLEncoder.encode(username, "UTF-8") +
-                    "&password=" + URLEncoder.encode(password, "UTF-8");
+                    "&client_id=" + URLEncoder.encode(clientId, StandardCharsets.UTF_8) +
+                    "&client_secret=" + URLEncoder.encode(clientSecret, StandardCharsets.UTF_8) +
+                    "&username=" + URLEncoder.encode(username, StandardCharsets.UTF_8) +
+                    "&password=" + URLEncoder.encode(password, StandardCharsets.UTF_8);
 
             // Set request headers
             con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
